ae9a061621835b3b9bf9996f3ade90e0049b57fa
helenemartin <helenemartin13@hotmail.com>
2021-09-27 11:33:37 +0100

    adding Jazzicon

--- components/EthName.js
+++ components/EthName.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { web3 } from "../lib/web3";
+import Jazzicon, { jsNumberForAddress } from "react-jazzicon";
 
 const EnsName = function ({ address }) {
   // TODO!
@@ -9,9 +10,11 @@ const EnsName = function ({ address }) {
   // get image if it has one
   // make jazzicon if not
   let formattedAddress = address.substr(0, 8) + "..." + address.substr(-4);
+
+  let icon = <Jazzicon diameter={32} seed={jsNumberForAddress(address)} />;
   return (
     <div className="eth-name">
-      <div className="icon">{/* icon goes here */}</div>
+      <div className="icon">{icon}</div>
 
       <div className="name">
         <span className="primary">{formattedAddress}</span>
